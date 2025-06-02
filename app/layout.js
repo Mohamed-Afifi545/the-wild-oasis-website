11e67166f6686b467deee0f4e1cf128eeab96240
MOHAMED AFIFI <mada2010545@gmail.com>
2025-06-02 19:05:48 +0300

    ok

--- app/layout.js
+++ app/layout.js
@@ -1,21 +1,33 @@
-import Logo from "./components/Logo";
-import Navigation from "./components/Navigation";
+import { Josefin_Sans } from "next/font/google";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+import "./_styles/globals.css";
+import Header from "./_components/Header";
 
 export const metadata = {
-  title: "The Wild Oasis",
-  description: "Welcome to paradise",
+  title: {
+    template: "%s || The Wild Oasis",
+    default: "Welcome || The Wild Oasis",
+  },
+  description:
+    "Luxurious cabin hotel, located in the heart of the Italian Dolomites, surrounded by beautiful mountains and dark forests.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
-        <footer>Copyright by the wild oasis</footer>
+      <body
+        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col antialiased relative `}
+        suppressHydrationWarning
+      >
+        <Header />
+        <div className="flex-1 px-8 py-12 grid">
+          <main className="max-w-7xl mx-auto w-full ">{children}</main>
+        </div>
       </body>
     </html>
   );
