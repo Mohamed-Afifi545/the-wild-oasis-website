a0cdcb152fdd687d1f1e7475d80e5c4cf4ae63fb
MOHAMED AFIFI <mada2010545@gmail.com>
2025-06-08 15:41:18 +0300

    good

--- app/_components/CabinList.js
+++ app/_components/CabinList.js
@@ -2,16 +2,37 @@ import { unstable_noStore as noStore } from "next/cache";
 import CabinCard from "./CabinCard";
 import { getCabins } from "../_lib/data-service";
 
-export default async function CabinList() {
-  noStore();
+export default async function CabinList({ filter }) {
+  // noStore();
 
   const cabins = await getCabins();
 
   if (!cabins.length) return null;
 
+  let displayedCabins;
+
+  switch (filter) {
+    case "all":
+      displayedCabins = cabins;
+      break;
+    case "small":
+      displayedCabins = cabins.filter((cabin) => cabin.maxCapacity <= 3);
+      break;
+    case "medium":
+      displayedCabins = cabins.filter(
+        (cabin) => cabin.maxCapacity >= 4 && cabin.maxCapacity <= 7
+      );
+      break;
+    case "large":
+      displayedCabins = cabins.filter((cabin) => cabin.maxCapacity >= 8);
+      break;
+    default:
+      displayedCabins = cabins;
+  }
+
   return (
     <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-      {cabins.map((cabin) => (
+      {displayedCabins.map((cabin) => (
         <CabinCard cabin={cabin} key={cabin.id} />
       ))}
     </div>
